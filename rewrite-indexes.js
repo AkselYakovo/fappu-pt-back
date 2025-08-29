6a2ed28b6bc64106b9d51ac76579166750e4f284
Axel Jacobo <jacobaxelth@outlook.com>
2025-08-29 12:58:27 -0600

    Update function call
    Update `rewriteFaultyIndexes()` so it passes `website` as a parameter
    
    Fix: #1

--- rewrite-indexes.js
+++ rewrite-indexes.js
@@ -2,7 +2,7 @@ const { rewriteFaultyIndexes } = require('./utils/record-porcelain')
 const website = process.argv[2].toUpperCase()
 
 function run() {
-  rewriteFaultyIndexes()
+  rewriteFaultyIndexes(website)
   return
 }
 
