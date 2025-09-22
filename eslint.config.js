dbf123a2ad1e8a8d8a22bfbd8a5433e28fb7c4b1
Axel Jacobo <jacobaxelth@outlook.com>
2025-09-22 06:58:56 -0600

    Create new config file
    Create new configuration file `eslint-config.js`. This file contain all
    the styling rules that will be enforced all over the project
    
    Closes: #10

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'rules': {
+    'no-console': 'warn',
+    'no-unused-vars': 'error',
+    'no-redeclare': 'error',
+    'no-regex-spaces': 'error',
+    'semi': ['warn', 'never'],
+    'quotes': ['warn', 'single'],
+    'no-unused-vars': 'error',
+    'no-trailing-spaces': 'error',
+  },
+}
