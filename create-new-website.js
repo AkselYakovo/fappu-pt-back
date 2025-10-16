64bbcc210562bf23b4b81c4f95d157b9abccd212
Axel Jacobo <jacobaxelth@outlook.com>
2025-10-16 08:43:40 -0600

    Add prompting
    Add prompting code to the process of creating a website
    
    See: #20

--- create-new-website.js
+++ create-new-website.js
@@ -6,6 +6,31 @@ const {
 } = require('./utils/file-creation')
 
 async function run() {
+  prompt.start()
+
+  const { web, website_code, url } = await prompt.get({
+    properties: {
+      web: {
+        description: 'Enter the title of the website:',
+        pattern: /\w{4,24}/i,
+        message: 'Use only letters. No spaces. Max 24 chars',
+        required: true
+      },
+      website_code: {
+        description: 'Enter a code for the website (letters only):',
+        pattern: /\w{3,6}/i,
+        message: 'Use only letter, on spaces. Max 6 chars',
+        required: true
+      },
+      url: {
+        description: 'Enter the link to the official page of the website:',
+        pattern:
+          /^(https?:\/\/)?([\w\-]+\.)+[\w\-]+(\/[\w\-._~:/?#[\]@!$&'()*+,;=]*)?$/i,
+        message: 'Please provide a valid URL'
+      }
+    }
+  })
+
   const data = {
     title: website,
     code: website.substring(0, 3)
